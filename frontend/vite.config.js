f0354fe5e9a484921a00ad31d8471e49ed429159
Loshmitha <vishnupriya0214@gmail.com>
2025-08-04 01:09:14 +0530

    vite updated config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    allowedHosts: process.env.ALLOWED_HOSTS ? 
+      process.env.ALLOWED_HOSTS.split(',') : 
+      ['localhost', '.onrender.com']
+  }
 })
